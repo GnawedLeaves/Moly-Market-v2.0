b552468fb7bb41661b6e6e557332d13e54c37c2e
kecheng <kecheng.guo@ncs.com.sg>
2026-04-04 17:39:04 +0800

    Fix bug where order cannot be retrieved by seller

--- backend/molymarket-product-order-service/src/main/java/com/nusiss/molymarket_product_order_service/controllers/OrderItemController.java
+++ backend/molymarket-product-order-service/src/main/java/com/nusiss/molymarket_product_order_service/controllers/OrderItemController.java
@@ -1,13 +1,20 @@
 package com.nusiss.molymarket_product_order_service.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nusiss.molymarket_product_order_service.dtos.OrderItemDto;
 import com.nusiss.molymarket_product_order_service.services.interfaces.OrderItemService;
+import com.nusiss.molymarket_product_order_service.services.interfaces.OrderService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product-order/order-items")
@@ -15,6 +22,20 @@ public class OrderItemController {
 
     @Autowired
     private OrderItemService orderItemService;
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/seller/{sellerId}")
+    @PreAuthorize("hasAnyAuthority('ROLE_SELLER')")
+    public ResponseEntity<?> getOrderItemsBySellerId(@PathVariable Long sellerId) {
+        try {
+            List<OrderItemDto> orderItems = orderService.getOrderItemsBySellerId(sellerId);
+            return new ResponseEntity<>(orderItems, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
 
     @PostMapping("/{id}/next")
     public ResponseEntity<String> nextState(@PathVariable Long id) {
